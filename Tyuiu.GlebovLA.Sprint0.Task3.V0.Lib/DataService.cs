f7b5b7be07839203224ed4fb169e0126c746b07b
Леонид Глебов <glebovleonid2008@gmail.com>
2026-09-18 20:04:00 +0500

    Task3

---
+++ Tyuiu.GlebovLA.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GlebovLA.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        //реализация метода Sum
+        public static int Sum(int a, int b)
+        { 
+            return a + b;
+        }
+        
+    }
+}
---
+++ Tyuiu.GlebovLA.Sprint0.Task3.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GlebovLA.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GlebovLA.Sprint0.Task3.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd31189d-a31f-4849-8f55-1a622cab8d03")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
